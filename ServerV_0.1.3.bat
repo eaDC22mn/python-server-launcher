6e4982973bd45a0c59f66568d19ddaf7a7479116
eaDC22mn <millenmjones@gmail.com>
2026-03-04 10:27:37 +0800

    Old versions
    old versions

---
+++ ServerV_0.1.3.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+cd C:\Server
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /c:"IPv4 Address"') do (
+    set "ipv4_address=%%a"
+    goto :found_ip
+)
+
+:found_ip
+echo Your IPv4 Address is: %ipv4_address%
+echo ---------------------------------------------------------------------------------
+echo Welcome to the simple server Hosting interface! This is the software interface for hosting a simple http python-based server. Began on 24/11/2025. 
+echo To access; As of 0.1.3, the form of access is: http//:%ipv4_address%:8000 [auto set] within a browser.
+echo All files and access is contained within the C:\Server directory.
+echo .
+echo Currently only operational on same-network access. No r/w capabilities.
+echo .
+echo ---------------------------------------------------------------------------------
+python -m http.server 8000
---
+++ ServerV_0.1.4.2.bat
@@ -0,0 +1,39 @@
+@echo offs
+:: Request administrative privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+setlocal
+color 1f
+echo color set [1f]
+cd C:\Server
+echo Server Dir set.
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /c:"IPv4 Address"') do (
+    set "ipv4_address=%%a"
+    goto :found_ip
+)
+mkdir C:\Server
+:found_ip
+color a
+echo IPv4 address found.
+echo Your IPv4 Address is: %ipv4_address%
+set "serverdir=C:\Server"
+color 1f
+echo ---------------------------------------------------------------------------------
+echo Welcome to the simple server Hosting interface! This is the software interface for hosting a simple http python-based server. Began on 24/11/2025. 
+echo.
+echo To access; As of 0.1.4.2, the form of access is: http//:%ipv4_address%:8000 [auto set] within a browser.
+echo All files and access is contained within the C:\Server directory.
+echo.
+
+echo.
+echo Currently only operational on same-network access. No r/w capabilities.
+echo User Directory currently: %Username%
+echo Server Directory currently: %serverdir%
+echo ---------------------------------------------------------------------------------
+python -m http.server 8000
+echo server started.
